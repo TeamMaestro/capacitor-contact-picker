348e6581bff7024fa8fe2be03c8cd4ddf844e77f
Sean Perkins <sean@meetmaestro.com>
2020-05-01 03:01:09 -0400

    fix(git): init project doesn't come with gitignore

--- dist/esm/web.d.ts
+++
@@ -1,8 +0,0 @@
-import { WebPlugin } from '@capacitor/core';
-import { ContactPickerPlugin } from './definitions';
-export declare class ContactPickerWeb extends WebPlugin implements ContactPickerPlugin {
-    constructor();
-    close(): Promise<void>;
-}
-declare const ContactPicker: ContactPickerWeb;
-export { ContactPicker };
